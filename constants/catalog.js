086c9796fca466997eef67115aa838c36cb8f976
lizaandrukhova <andruhovaelizaveta685@gmail.com>
2022-07-30 16:06:18 +0300

    initial commit

---
+++ constants/catalog.js
@@ -0,0 +1,56 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Baggini',
+        img: 'https://a.lmcdn.ru/product/B/A/BA039BWMCMG3_13322343_2_v1.jpg',
+        price: 870,
+    },
+    {
+        id: 'el2',
+        name: 'Roberto Buono',
+        img: 'https://a.lmcdn.ru/product/R/O/RO046BWHSZN1_10129990_1_v1.jpg',
+        price: 1760,
+    },
+    {
+        id: 'el3',
+        name: 'Roberto Buono',
+        img: 'https://a.lmcdn.ru/product/R/O/RO046BWEOLB3_8246750_1_v1.jpg',
+        price: 2132,
+    },
+    {
+        id: 'el4',
+        name: 'Bella Bertucci',
+        img: 'https://a.lmcdn.ru/product/M/P/MP002XW07UYY_14569290_1_v1.jpeg',
+        price: 1785,
+    },
+    {
+        id: 'el5',
+        name: 'Elsi',
+        img: 'https://a.lmcdn.ru/img600x866/E/L/EL026BWKYGK8_12256141_1_v1.jpg',
+        price: 1196,
+    },
+    {
+        id: 'el6',
+        name: 'Bella Bertucci',
+        img: 'https://a.lmcdn.ru/img600x866/M/P/MP002XW07RXT_14532451_1_v3.jpg',
+        price: 1585,
+    },
+    {
+        id: 'el7',
+        name: 'Bella Bertucci',
+        img: 'https://a.lmcdn.ru/product/M/P/MP002XW07UZB_14569319_1_v1.jpeg',
+        price: 1685,
+    },
+    {
+        id: 'el8',
+        name: 'Bella Bertucci',
+        img: 'https://a.lmcdn.ru/product/M/P/MP002XW07UY6_14569135_1_v1.jpeg',
+        price: 1785,
+    },
+    {
+        id: 'el9',
+        name: 'Zadig & Voltaire',
+        img: 'https://a.lmcdn.ru/img600x866/M/P/MP002XW03ORR_12518483_1_v1.jpeg',
+        price: 10600,
+    },
+]
